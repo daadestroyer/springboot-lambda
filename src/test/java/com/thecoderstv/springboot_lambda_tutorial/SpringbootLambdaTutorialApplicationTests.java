f0df7fc79a2528ddbdcfd50ddf1512789badf52f
daadestroyer <nigamshubham2000@gmail.com>
2025-05-16 22:42:57 +0530

    code added,,,

---
+++ src/test/java/com/thecoderstv/springboot_lambda_tutorial/SpringbootLambdaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecoderstv.springboot_lambda_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLambdaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
